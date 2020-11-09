b37bbdf2e1f2883a86264efa9977f9957789840c
Fiona Yuan <Fiona.Yuan@aspiraconnect.com>
2020-11-09 20:23:10 +0800

    main commit

--- frame/main.py
+++ frame/main.py
@@ -6,6 +6,8 @@ from frame.market import Market
 
 class Main(BasePage):
     def gogo_market(self):
-        self.find(By.XPATH, "//*[resource-id='']").click()
-        self.find(By.XPATH, "//*[@resource=id='android:id/tabs']//*[@text='行情']").click()
+        # self.find(By.XPATH, "//*[resource-id='']").click()
+        # self.find(By.XPATH, "//*[@resource=id='android:id/tabs']//*[@text='行情']").click()
+
+        self.parse_yaml("./main.yaml", "goto_market")
         return Market(self.driver)
